336aa64c25583f830bfcc3fdeeb256635c084b58
seantyh <seantyh@gmail.com>
2022-10-18 16:29:15 +0800

    update layout in search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -161,10 +161,10 @@ const SearchPage: NextPage<SearchPageProps> = ({ searchResults, searchT }) => {
   const [videoUrl, setVideoUrl] = useState("");
   const [seekToSec, setSeekToSec] = useState(0);
   const [handSelect, setHandSelect] = useState("");
-  const [soundSelect, setSoundSelect] = useState("");    
+  const [soundSelect, setSoundSelect] = useState("");
   const [annotationSpans, setAnnotationSpans] = useState<AnnotationSpans>(JSON.parse(searchResults));
 
-  const selectedSpans = annotationSpans.filter((x)=>x.name == getVideoName(videoUrl));
+  const selectedSpans = annotationSpans.filter((x) => x.name == getVideoName(videoUrl));
 
   const playerRef: React.MutableRefObject<VideoJsPlayer> = React.useRef(null);
   const videoJsOptions = {
@@ -224,24 +224,24 @@ const SearchPage: NextPage<SearchPageProps> = ({ searchResults, searchT }) => {
     seekToSec: number
   ) {
     setVideoUrl(videoUrl);
-    setSeekToSec(seekToSec);    
+    setSeekToSec(seekToSec);
   }
 
   function onSelectedSpansUpdated(selSpans: AnnotationSpans) {
     const videoName = getVideoName(videoUrl);
-    const baseIndex = annotationSpans.findIndex((x)=>x.name == videoName);    
+    const baseIndex = annotationSpans.findIndex((x) => x.name == videoName);
     let newSpans = annotationSpans;
     if (baseIndex >= 0) {
-      newSpans = annotationSpans.filter((x)=>x.name!=videoName);
+      newSpans = annotationSpans.filter((x) => x.name != videoName);
       console.log("selected Spans: ", newSpans);
       newSpans.splice(baseIndex, 0, ...selSpans);
     }
 
-    console.log("newSpans: ",  newSpans);
+    console.log("newSpans: ", newSpans);
     setAnnotationSpans(newSpans);
   }
 
-  
+
   return (
     <>
       <Head>
@@ -257,120 +257,132 @@ const SearchPage: NextPage<SearchPageProps> = ({ searchResults, searchT }) => {
         style={{ minHeight: "100vh", }}>
         {/* <Container className="mt-5" maxWidth="xl"> */}
         <Container maxWidth="xl">
-          <form
-            action="search" method="GET"
+          <Grid2
+            display="flex"
+            container
+            spacing={5}
+            sx={{ flexDirection: "row" }}
           >
-            <Grid2
-              container
-              spacing={1}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Grid2 xs={12}>
-                <TextField
-                  fullWidth
-                  label="搜尋文字、聲音、手勢"
-                  id="search"
-                  type="text"
-                  name="query"
-                  value={queryText}
-                  onChange={handleChange}
-                  InputLabelProps={{ shrink: true }}
-                  InputProps={{ endAdornment: <Button type="submit" variant="contained" disableElevation><SearchIcon /></Button> }}
-                  required
-                />
-              </Grid2>
-            </Grid2>
-            <Grid2
-              container
-              spacing={1}
-              justifyContent="left"
-              alignItems="center"
-            >
-              <Grid2 xs={12} md={3}>
-                <FormControl>
-                  <FormLabel id="demo-radio-buttons-group-label">Search Type</FormLabel>
-                  <RadioGroup
-                    row
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    value={searchType}
-                    onChange={(e) => setSearchType(e.target.value)}
-                    name="searchType"
-                  >
-                    <FormControlLabel value="asr" control={<Radio />} label="ASR" />
-                    <FormControlLabel value="ocr" control={<Radio />} label="OCR" />
-                    <FormControlLabel value="blank" control={<Radio />} label="Blank" />
-                  </RadioGroup>
-                </FormControl>
-              </Grid2>
-              <Grid2 xs={6} sm={2} >
-                <FormControl sx={{ mt: 2, minWidth: 120 }} size="small">
-                  <InputLabel id="hand-select-label">手勢</InputLabel>
-                  <Select
-                    labelId="hand-select-label"
-                    id="hand-select"
-                    value={handSelect}
-                    label="手勢"
-                    onChange={(e) => setHandSelect(e.target.value)}
-                    autoWidth
-                  >
-                    <MenuItem value="hand-moving">手揮動</MenuItem>
-                    <MenuItem value="hand-palm-visible">手掌可見</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid2>
+            <Grid2 xs={6} md={6} lg={7}>
+              <form
+                action="search" method="GET"
+              >
+                <Grid2
+                  container
+                  spacing={1}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Grid2 xs={12}>
+                    <TextField
+                      fullWidth
+                      label="搜尋文字、聲音、手勢"
+                      id="search"
+                      type="text"
+                      name="query"
+                      value={queryText}
+                      onChange={handleChange}
+                      InputLabelProps={{ shrink: true }}
+                      InputProps={{ endAdornment: <Button type="submit" variant="contained" disableElevation><SearchIcon /></Button> }}
+                      required
+                    />
+                  </Grid2>
+                </Grid2>
+                <Grid2
+                  container
+                  spacing={1}
+                  justifyContent="left"
+                  alignItems="center"
+                >
+                  <Grid2 md={12} display="flex" sx={{ flexDirection: "row" }}>
+                    <FormControl>
+                      <FormLabel id="demo-radio-buttons-group-label">Search Type</FormLabel>
+                      <RadioGroup
+                        row={true}
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        value={searchType}
+                        onChange={(e) => setSearchType(e.target.value)}
+                        name="searchType"
+                      >
+                        <FormControlLabel value="asr" control={<Radio />} label="ASR" />
+                        <FormControlLabel value="ocr" control={<Radio />} label="OCR" />
+                        <FormControlLabel value="blank" control={<Radio />} label="Blank" />
+                      </RadioGroup>
+                    </FormControl>
+                  </Grid2>
+                  <Grid2 md={6} >
+                    <FormControl sx={{ mt: 2, minWidth: 120 }} size="small">
+                      <InputLabel id="hand-select-label">手勢</InputLabel>
+                      <Select
+                        labelId="hand-select-label"
+                        id="hand-select"
+                        value={handSelect}
+                        label="手勢"
+                        onChange={(e) => setHandSelect(e.target.value)}
+                        autoWidth
+                      >
+                        <MenuItem value="hand-moving">手揮動</MenuItem>
+                        <MenuItem value="hand-palm-visible">手掌可見</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid2>
 
-              <Grid2 xs={6} sm={2} >
-                <FormControl sx={{ mt: 2, minWidth: 120 }} size="small">
-                  <InputLabel id="sound-select-label">語音</InputLabel>
-                  <Select
-                    labelId="sound-select-label"
-                    id="sound-select"
-                    value={soundSelect}
-                    label="語音"
-                    onChange={(e) => setSoundSelect(e.target.value)}
-                    autoWidth
-                  >
-                    <MenuItem value="sound-overlapping">語音重疊</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid2>
+                  <Grid2 md={6} >
+                    <FormControl sx={{ mt: 2, minWidth: 120 }} size="small">
+                      <InputLabel id="sound-select-label">語音</InputLabel>
+                      <Select
+                        labelId="sound-select-label"
+                        id="sound-select"
+                        value={soundSelect}
+                        label="語音"
+                        onChange={(e) => setSoundSelect(e.target.value)}
+                        autoWidth
+                      >
+                        <MenuItem value="sound-overlapping">語音重疊</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid2>
+                </Grid2>
+              </form>
             </Grid2>
-          </form>
-
+            {annotationSpans ?
+              <Grid2 xs={6} md={6} lg={5} display="flex"
+                justifyContent="center"
+                alignContent="center"
+                sx={{
+                  position: "fixed",
+                  right: "10px"
+                }}>
+                {videoUrl && selectedSpans &&
+                  <VideoView
+                    video_url={videoUrl}
+                    seekToSec={seekToSec}
+                    annotSpans={selectedSpans}
+                    toShowOcr={false}
+                    onAnnotSpansUpdated={onSelectedSpansUpdated} />
+                }
+              </Grid2>
+              :
+              <div />
+            }
+          </Grid2>
           <Grid2
             container
             spacing={5}
           >
             {annotationSpans ?
-              <>
-                <Grid2 mdOffset={2} xs={12} md={8} display="flex"
-                  justifyContent="center"
-                  alignContent="center"
-                  sx={{
-                    position: "sticky",
-                    top: "10px",                    
-                  }}>
-                  {videoUrl && selectedSpans &&
-                    <VideoView
-                      video_url={videoUrl}
-                      seekToSec={seekToSec}
-                      annotSpans={selectedSpans}
-                      toShowOcr={false}
-                      onAnnotSpansUpdated={onSelectedSpansUpdated} />
-                  }
-                </Grid2>
-                <Grid2 xs={12}>
-                  <CorpusResult
-                    annotationSpans={annotationSpans}
-                    setAnnotationSpans={setAnnotationSpans}
-                    player={playerRef}
-                    queryText={queryText}
-                    searchType={searchType}
-                    onSelectedSpanChanged={onSelectedSpanChanged}
-                  />
-                </Grid2>
-              </>
+
+              <Grid2 xs={12}>
+                <CorpusResult
+                  annotationSpans={annotationSpans}
+                  setAnnotationSpans={setAnnotationSpans}
+                  player={playerRef}
+                  queryText={queryText}
+                  searchType={searchType}
+                  onSelectedSpanChanged={onSelectedSpanChanged}
+                />
+              </Grid2>
+
               :
               <Grid2 xs={12} display="flex" justifyContent="center">
                 <Typography variant="h5">Please make a search!</Typography>
